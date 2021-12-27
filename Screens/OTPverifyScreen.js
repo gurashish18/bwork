7700ad5d5511a2b8f1c353fd0a83fc3f6bd7e80f
Gurashish <ggill_be19@thapar.edu>
2021-12-27 08:22:55 +0530

    spinner

--- Screens/OTPverifyScreen.js
+++ Screens/OTPverifyScreen.js
@@ -4,17 +4,20 @@ import OTPInputView from '@twotalltotems/react-native-otp-input'
 import {useNavigation} from '@react-navigation/native'
 import Button from '../components/Button'
 import auth from '@react-native-firebase/auth';
+import Spinner from 'react-native-loading-spinner-overlay';
 
 const OTPverifyScreen = ({ route: { params: { phoneNumber } }}) => {
     const navigation = useNavigation()
     const [otp, setotp] = useState('')
     const [confirm, setConfirm] = useState(null);
+    const[spinner, setspinner] = useState(false) 
 
     useEffect(() => {
       signInWithPhoneNumber();
     }, [])
 
     async function signInWithPhoneNumber() {
+      setspinner(true)
       try{
          const code = '+91'
          const pno = code.concat(" ", phoneNumber)
@@ -24,9 +27,10 @@ const OTPverifyScreen = ({ route: { params: { phoneNumber } }}) => {
        }catch(e){
           console.log(e)
       }
+      setspinner(false)
      }
      async function confirmCode() {
-       console.log(otp)
+      setspinner(true)
       try{
         const response = await confirm.confirm(otp);
         if(response){
@@ -35,9 +39,15 @@ const OTPverifyScreen = ({ route: { params: { phoneNumber } }}) => {
       } catch(e){
           console.log(e)
       }
+      setspinner(false)
     }
     return (
         <View style={{flex:1, backgroundColor: '#000000', alignItems: 'center', justifyContent: 'center'}}>
+            <Spinner
+              visible={spinner}
+              textContent={'Loading...'}
+              textStyle={{}}
+            />
             <Text style={{fontSize: 20, color: '#ffffff'}}>Enter code here</Text>
             <OTPInputView
                 style={{width: '80%', height: 200}}
